5aa4c5811c84dd6e3dae342348ec66ba84809b67
Adam Jackman <adam@acjackman.com>
2019-03-19 18:35:45 -0600

    Remove extra test

--- tests/test_cli_info.py
+++ tests/test_cli_info.py
@@ -7,15 +7,6 @@ import pytest
 from toggl2harvest.scripts.toggl2harvest import cli
 
 
-def test_can_test_cli(cli_runner, credentials_file):
-    result = cli_runner.invoke(cli, ['info'])
-
-    print(result.output)
-    assert result.exit_code == 0, result.output
-
-    assert f'Configuration Directory: "."' in result.output
-
-
 def test_can_set_config_dir_with_option(cli_runner, tmpdir, credentials_file):
     result = cli_runner.invoke(cli, [f'--config-dir={tmpdir}', 'info'])
 
@@ -23,10 +14,10 @@ def test_can_set_config_dir_with_option(cli_runner, tmpdir, credentials_file):
     assert f'Configuration Directory: "{tmpdir}"' in result.output
 
 
-@pytest.mark.runner_setup(env={'TOGGL2HARVEST_CONFIG': '~/.toggl2harvest'})
+@pytest.mark.runner_setup(env={'TOGGL2HARVEST_CONFIG': '~/.some_dot_folder'})
 def test_can_set_config_dir_with_env_var(cli_runner, credentials_file):
     result = cli_runner.invoke(cli, ['info'])
 
-    full_path = expanduser('~/.toggl2harvest')
+    full_path = expanduser('~/.some_dot_folder')
     assert result.exit_code == 0, result.output
     assert f'Configuration Directory: "{full_path}"' in result.output
