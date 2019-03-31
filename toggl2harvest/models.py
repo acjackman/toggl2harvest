ee1c2e1d2037f3fefd4cb9987adcec2f9595378c
Adam Jackman <adam@acjackman.com>
2019-03-30 19:38:23 -0600

    Core Time Log Model

--- toggl2harvest/models.py
+++ toggl2harvest/models.py
@@ -2,3 +2,11 @@ class TimeEntry:
     def __init__(self, start, end):
         self.start = start
         self.end = end
+
+
+class TimeLog:
+    def __init__(self, project_code, description, is_billable, time_entries):
+        self.project_code = project_code
+        self.description = description
+        self.is_billable = is_billable
+        self.time_entries = time_entries
--- toggl2harvest/schemas.py
+++ toggl2harvest/schemas.py
@@ -11,3 +11,14 @@ class TimeEntrySchema(Schema):
     @post_load
     def make_user(self, data):
         return models.TimeEntry(**data)
+
+
+class TimeLogSchema(Schema):
+    project_code = fields.Str(allow_none=True)
+    description = fields.Str(allow_none=True)
+    is_billable = fields.Boolean(required=True)
+    time_entries = fields.Nested(TimeEntrySchema, allow_none=True, many=True)
+
+    @post_load
+    def make_user(self, data):
+        return models.TimeLog(**data)
